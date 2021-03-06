4498bca7189db855cc22efa6a22e76836bc6475e
Yoonkun Lee <ryan.yoonkun.lee@gmail.com>
2021-03-07 01:34:23 +1300

    Updated home page mobile view

--- MyWebBlog.Web/wwwroot/js/blog.js
+++ MyWebBlog.Web/wwwroot/js/blog.js
@@ -1,7 +1,1 @@
-﻿jQuery(document).ready(function () {
-    var lang = $('#imageSelect').val();
-
-    if (lang == "JQuery") {
-        $('.title').css('background-image', 'url(../../images/blog/12.jpg')
-    }
-});
+﻿
